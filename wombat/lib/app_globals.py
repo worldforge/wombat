05893dd5a99d21e519bdddd0d6cbc2be9811db45
Kai Blin <kai@samba.org>
2008-07-02 22:57:26 +0200

    Getting ready for 0.0.3

--- wombat/lib/app_globals.py
+++ wombat/lib/app_globals.py
@@ -11,4 +11,4 @@ class Globals(object):
         initialization and is available during requests via the 'g'
         variable
         """
-        self.version = "0.0.2"
+        self.version = "0.0.3"
