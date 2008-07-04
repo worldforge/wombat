5fc037e7b263c56e102301cb7278306da4d3a23d
Kai Blin <kai@samba.org>
2008-07-04 13:54:36 +0200

    Prepare for 0.0.4 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='wombat',
-    version="0.0.3",
+    version="0.0.4",
     description='WorldForge Open Media Browser/Archive Tool',
     author='Kai Blin',
     author_email='kai.blin@gmail.com',
