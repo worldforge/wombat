f7f5725e42ceb23d0062f9faefc4ee8b94f0755c
Kai Blin <kai@samba.org>
2008-09-21 18:26:24 +0200

    websetup: Support adding a superuser account on setup-app

--- wombat/websetup.py
+++ wombat/websetup.py
@@ -1,5 +1,6 @@
 """Setup the wombat application"""
 import logging
+import sys
 
 from paste.deploy import appconfig
 from pylons import config
@@ -7,6 +8,22 @@ from pylons import config
 from wombat.config.environment import load_environment
 
 log = logging.getLogger(__name__)
+
+def setup_superuser(model):
+    """Set up the superuser account"""
+    import md5
+    s = model.Session()
+    print "Enter email for the super user"
+    email = unicode(sys.stdin.readline().strip())
+    print "Enter superuser password"
+    passwd = unicode(md5.md5(unicode(sys.stdin.readline().strip())).hexdigest())
+    user = model.User(email, passwd, True)
+    s.save(user)
+    data = model.UserData(u"System Administrator", u"admin")
+    data.user = user
+    #TODO: set up as role "admin"
+    s.save(data)
+    s.commit()
 
 def setup_config(command, filename, section, vars):
     """Place any commands to setup wombat here"""
@@ -27,4 +44,17 @@ def setup_config(command, filename, section, vars):
     log.info("Creating tables...")
     model.metadata.create_all(bind=engine)
     log.info("Successfully set up.")
+
+    answer = ""
+
+    if config['app_conf']['create_superuser'] == "false":
+        answer = "n"
+
+    while not answer in ("y", "n"):
+        print "Set up superuser account now? (Y/N)"
+        answer = sys.stdin.readline().strip().lower()
+
+    if answer == "y":
+        setup_superuser(model)
+
 
