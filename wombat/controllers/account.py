a59590f03eb2664d0d0e1817524be34211d1185d
Kai Blin <kai@samba.org>
2008-09-26 22:22:01 +0200

    account: Add "edit" action to account controller

--- wombat/controllers/account.py
+++ wombat/controllers/account.py
@@ -137,6 +137,11 @@ class AccountController(BaseController):
         if id is None:
             abort(404)
 
+        if 'messages' in session:
+            c.messages += session['messages']
+            del session['messages']
+            session.save()
+
         c.account = c.session.query(User).get(id)
         if c.account is None:
             abort(404)
@@ -145,8 +150,115 @@ class AccountController(BaseController):
             abort(403)
 
         #TODO: Admin users should be able to edit accounts as well.
-        if session['user'].id != c.account.id:
+        if session['user'] != c.account.id:
             abort(403)
 
         return render('/derived/account/edit.html')
+
+    def change(self, id):
+        user_email = unicode(request.params.get('user_email'))
+        user_email_c = unicode(request.params.get('user_email_confirm'))
+
+        if not 'user' in session:
+            abort(403)
+
+        s = Session()
+        user = s.query(User).get(session['user'])
+        if user is None:
+            abort(404)
+
+        if user_email != user_email_c:
+            if id == "ajax":
+                return "email address mismatch"
+            else:
+                session['messages'] = ["Email address mismatch"]
+                session.save()
+                redirect_to(action="edit")
+
+        if not self._is_email_valid(user_email):
+            if id == "ajax":
+                return "invalid email address"
+            else:
+                session['messages'] = ["Invalid email address"]
+                session.save()
+                redirect_to(action="edit")
+
+        # check if the email matches the current user's email
+        u_by_email = s.query(User).filter_by(email=user_email).first()
+
+        if u_by_email is not None:
+            if u_by_email.id != user.id:
+                if id == "ajax":
+                    return "email already associated with an account"
+                else:
+                    session['messages'] = ["Email already associated with an account"]
+                    session.save()
+                    redirect_to(action="edit")
+
+        user.email = user_email
+
+        user_pass = unicode(request.params.get('user_pass'))
+        user_pass_c = unicode(request.params.get('user_pass_confirm'))
+
+        if user_pass != user_pass_c:
+            if id == "ajax":
+                return "password mismatch"
+            else:
+                session['messages'] = ["Password mismatch"]
+                session.save()
+                redirect_to(action="edit")
+
+        if user_pass != "":
+            user.password = unicode(md5.md5(user_pass).hexdigest())
+
+        if request.params.get('user_vcs_pass') is not None:
+            vcs_pass = unicode(request.params.get('user_vcs_pass'))
+            vcs_pass_c = unicode(request.params.get('user_vcs_pass_confirm'))
+
+            if vcs_pass != vcs_pass_c:
+                if id == "ajax":
+                    return "VCS password mismatch"
+                else:
+                    session['messages'] = ["VCS password mismatch"]
+                    session.save()
+                    redirect_to(action="edit")
+
+                user.user_data.vcs_pass = vcs_pass
+
+        if request.params.get('user_name') is not None:
+            user_name = unicode(request.params.get('user_name'))
+        else:
+            user_name = u"Unnamed User"
+
+        user.user_data.name = user_name
+
+        if request.params.get('user_nick') is not None:
+            user_nick = unicode(request.params.get('user_nick'))
+        else:
+            user_nick = u"anonymous"
+
+        user.user_data.nick = user_nick
+
+        if request.params.get('user_vcs_user') is not None:
+            vcs_user = unicode(request.params.get('user_vcs_user'))
+            user.user_data.vcs_user = vcs_user
+        else:
+            vcs_user = None
+
+        s.update(user)
+        s.commit()
+
+        if id == "ajax":
+            return "user data updated"
+        else:
+            redirect_to(action='changed', id=None)
+
+    def changed(self):
+        c.name = config['app_conf']['site_name']
+        c.title = 'Updated Account'
+        c.messages = []
+        c.session = Session()
+
+        return render('derived/account/changed.html')
+
 
--- wombat/tests/functional/test_account.py
+++ wombat/tests/functional/test_account.py
@@ -74,10 +74,12 @@ class TestAccountController(TestController):
         s = model.Session()
         user = s.query(model.User).filter_by(email=u"test@localhost").first()
 
+        self.assertTrue(user is not None)
         self.assertEqual(user.user_data.name, u"Test Tester")
         self.assertEqual(user.user_data.nick, u"test")
         self.assertEqual(user.user_data.vcs_user, u"test")
         self.assertEqual(user.user_data.vcs_pass, u"test")
+        self.assertFalse(user.active)
 
         # and now try to register the same user, should fail
         form['user_email'] = "test@localhost"
@@ -88,3 +90,111 @@ class TestAccountController(TestController):
 
         res = new_res.follow()
         res.mustcontain("Email already associated with an account")
+
+    def test_index(self):
+        res = self.app.get(url_for(controller='account'))
+
+        self.assertEqual(res.c.accounts, [])
+
+        s = model.Session()
+        # create a user that's already active
+        user = model.User(u"test@localhost", unicode(md5.md5(u"secret").hexdigest()), True)
+        data = model.UserData(u"Test Testus", u"test", u"test", u"test")
+        data.user = user
+        s.save(user)
+        s.save(data)
+        s.commit()
+
+        res = self.app.get(url_for(controller='account'))
+
+        self.assertEqual(len(res.c.accounts), 1)
+        acc = res.c.accounts[0]
+        self.assertEqual(acc.id, user.id)
+
+    def test_edit(self):
+        res = self.app.get(url_for(controller='account', action='edit'),
+                status=404)
+
+        res = self.app.get(url_for(controller='account', action='edit', id=1),
+                status=404)
+
+        s = model.Session()
+        # create a user that's already active
+        user = model.User(u"test@localhost", unicode(md5.md5(u"secret").hexdigest()), True)
+        data = model.UserData(u"Test Testus", u"test", u"test", u"test")
+        data.user = user
+        s.save(user)
+        s.save(data)
+        s.commit()
+
+        res = self.app.get(url_for(controller='account', action='edit', id=1),
+                status=403)
+
+        tmp_res = self.app.post(url_for(controller='auth', action='submit', id='ajax'),
+                params={'email':'test@localhost', 'password':'secret'})
+        tmp_res.mustcontain("success")
+
+
+        res = self.app.get(url_for(controller='account', action='edit', id=1))
+
+        form = None
+        for key in res.forms.keys():
+            if 'user_email' in res.forms[key].fields:
+                form = res.forms[key]
+
+        self.assertNotEqual(form, None)
+
+        # check the default values are ok
+        self.assertEqual(form['user_email'].value, user.email)
+        self.assertEqual(form['user_email_confirm'].value, user.email)
+        self.assertEqual(form['user_name'].value, data.name)
+        self.assertEqual(form['user_nick'].value, data.nick)
+        self.assertEqual(form['user_vcs_user'].value, data.vcs_user)
+
+        # Now try to save changes
+        # first do it wrong
+        form['user_email'] = "testus"
+        form['user_email_confirm'] = "testus"
+
+        new_res = form.submit()
+        res = new_res.follow()
+        res.mustcontain("Invalid email address")
+
+        form['user_email'] = "test@localhost"
+        form['user_email_confirm'] = "test@test"
+
+        new_res = form.submit()
+        res = new_res.follow()
+        res.mustcontain("Email address mismatch")
+
+        form = None
+        for key in res.forms.keys():
+            if 'user_email' in res.forms[key].fields:
+                form = res.forms[key]
+
+        self.assertNotEqual(form, None)
+
+        form['user_pass'] = "topsecret"
+
+        new_res = form.submit()
+        res = new_res.follow()
+        res.mustcontain("Password mismatch")
+
+        # finally do everything right
+
+        form = None
+        for key in res.forms.keys():
+            if 'user_email' in res.forms[key].fields:
+                form = res.forms[key]
+
+        self.assertNotEqual(form, None)
+        form['user_nick'] = "testus"
+
+        new_res = form.submit()
+        res = new_res.follow()
+        res.mustcontain("Account information successfully updated")
+
+        user = s.query(model.User).get(user.id)
+
+        self.assertEqual(user.user_data.nick, u"testus")
+
