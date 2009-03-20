3c01d18d7e842c877487689e6a3902c53e841911
Kai Blin <kai@samba.org>
2009-03-20 09:01:23 +0100

    database: Add a backup script for the database.
    The script stores content-specific data and meta data in different files,
    allowing e.g. to dump the files and keep the users easily.

---
+++ backup_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DB=${1}
+BACKUP_PREFIX=${2}
+
+sqlite3 ${DB} ".dump revisions" > ${BACKUP_PREFIX}_content.sql
+sqlite3 ${DB} ".dump dirs" >> ${BACKUP_PREFIX}_content.sql
+sqlite3 ${DB} ".dump files" >> ${BACKUP_PREFIX}_content.sql
+sqlite3 ${DB} ".dump assets" >> ${BACKUP_PREFIX}_content.sql
+sqlite3 ${DB} ".dump collections" >> ${BACKUP_PREFIX}_content.sql
+
+sqlite3 ${DB} ".dump users" > ${BACKUP_PREFIX}_meta.sql
+sqlite3 ${DB} ".dump user_data" >> ${BACKUP_PREFIX}_meta.sql
+sqlite3 ${DB} ".dump roles" >> ${BACKUP_PREFIX}_meta.sql
+sqlite3 ${DB} ".dump user_roles" >> ${BACKUP_PREFIX}_meta.sql
+sqlite3 ${DB} ".dump email_confirm" >> ${BACKUP_PREFIX}_meta.sql
+sqlite3 ${DB} ".dump reset_data" >> ${BACKUP_PREFIX}_meta.sql
