90c0ecaadb0f0d63d0f69e094e0624edcfc2eaf7
Kai Blin <kai@samba.org>
2009-11-23 20:34:17 +0100

    Prepare 0.5.0pre1 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='wombat',
-    version="0.4.5",
+    version="0.5.0",
     description='WorldForge Open Media Browser - Artist Tool',
     author='Kai Blin',
     author_email='kai.blin@gmail.com',
--- wombat/lib/app_globals.py
+++ wombat/lib/app_globals.py
@@ -14,7 +14,7 @@ class Globals(object):
         initialization and is available during requests via the 'g'
         variable
         """
-        self.version = "0.4.5"
+        self.version = "0.5.0pre1"
 
         self.last_cleanup = "Never"
         self.cleanup_timer = Timer(60.0, cleanup_dbs, [self])
